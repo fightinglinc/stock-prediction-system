87779c0cb737402e2075e7303dcc21d90bd7c690
Lincoin <linchnx@163.com>
2019-05-03 00:29:05 -0400

    Fix predict page bug

--- public/js/predict.js
+++ public/js/predict.js
@@ -4,23 +4,25 @@ $(document).ready(function () {
         let company = $('#selectCompany').val();
         let term = $('#selectTerm').val();
 
-        $('#predictPrice').dataTable({
-            "ajax": 'http://localhost:5000/predict/' + company + '/' + term,
-            "processing": true,
-            "lengthChange": false,
-            "info": false,
-            "searching": false,
-            "paging": false,
-            "bDestroy": true,
-            columns: [
-                {data: 'algorithm'},
-                {data: 'price'},
-            ],
-        }).on('xhr.dt', function (e, settings, json, xhr) {
-            if (xhr.status === 0) {
-                swal("Not exist", "Please select another stock", "warning");
-                $('#predictPrice tbody').html('');
-            }
-        });
+        if (company && term) {
+            $('#predictPrice').dataTable({
+                "ajax": 'http://localhost:5000/predict/' + company + '/' + term,
+                "processing": true,
+                "lengthChange": false,
+                "info": false,
+                "searching": false,
+                "paging": false,
+                "bDestroy": true,
+                columns: [
+                    {data: 'algorithm'},
+                    {data: 'price'},
+                ],
+            }).on('xhr.dt', function (e, settings, json, xhr) {
+                if (xhr.status === 0) {
+                    swal("Not exist", "Please select another stock", "warning");
+                    $('#predictPrice tbody').html('');
+                }
+            });
+        }
     });
 });
